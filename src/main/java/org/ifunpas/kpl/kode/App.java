0de76994447cb51cf9f9639d454bf95c045baf9e
admin604 <admin604@DESKTOP-MA62GBM>
2018-11-24 07:22:15 +0700

    coba

---
+++ src/main/java/org/ifunpas/kpl/kode/App.java
@@ -0,0 +1,13 @@
+package org.ifunpas.kpl.kode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
